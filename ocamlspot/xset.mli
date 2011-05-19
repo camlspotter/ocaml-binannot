0114d9160b7b35a4289ea645c06edd812cb71514
Jun FURUSE <jun.furuse@sc.com>
2011-05-20 08:31:04 +0900

    comment

--- ocamlspot/xset.mli
+++ ocamlspot/xset.mli
@@ -4,7 +4,7 @@
 (*                                                                     *)
 (*                             Jun FURUSE                              *)
 (*                                                                     *)
-(*   Copyright 2008, 2009 Jun Furuse. All rights reserved.             *)
+(*   Copyright 2008-2011 Jun Furuse. All rights reserved.              *)
 (*   This file is distributed under the terms of the GNU Library       *)
 (*   General Public License, with the special exception on linking     *)
 (*   described in file LICENSE.                                        *)
@@ -19,7 +19,12 @@ module type S = sig
   include Set.S
 
   val middle : t -> elt option
+  (** Obtain the top element in the binary tree representation of [t],
+      which is usually near the median. *)
+
   val find : elt -> t -> elt option
+  (** find the "equal" element as [elt] in [t] *)
+
 end
 
 module Make(Ord : OrderedType) : S 
