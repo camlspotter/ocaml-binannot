5b719ac292beda60d968f9d080d31ed8dc804a7d
Jun FURUSE <jun.furuse@sc.com>
2011-06-13 18:40:23 +0900

    tests

---
+++ ocamlspot/newtests/new_class_new.ml
@@ -0,0 +1,3 @@
+class (* c => *) c = object end (* <= c *)
+
+let o : c (* ? c *) = new c (* ? c *)
---
+++ ocamlspot/newtests/new_class_param.ml
@@ -0,0 +1,4 @@
+class c ((* p => *) p (* <= p *) : int) = object
+  val x = p (* ? p *) 
+  method get_p = p (* ? p *)
+end
